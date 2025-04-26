0d45c59b95979de69fd5c3c380812eeb49afd9be
keita.muramatsu <keita.muramatsu@sozonext.com>
2025-04-27 00:41:07 +0900

    no message

--- script.js
+++ script.js
@@ -2,72 +2,90 @@ const SIP_SERVER = "sip.inntouch.jp";
 const SIP_DOMAIN = "its2.sozonext.com";
 const EXTENSION_NUMBER = "101";
 const EXTENSION_PASSWORD = "p@ssw0rd";
-const EXTENSION_NUMBER_ = "801";
+const TARGET_EXTENSION_NUMBER = "801";
 
 // Swiper
 const swiper = new Swiper(".swiper", {
   loop: true,
   speed: 2 * 1000,
-  autoplay: {
-    delay: 10 * 1000,
-  },
+  autoplay: { delay: 10 * 1000 },
 });
 
 /**
  * Event
  */
 window.onload = () => {
-  register();
+  Android.register(SIP_SERVER, SIP_DOMAIN, EXTENSION_NUMBER, EXTENSION_PASSWORD);
 };
-
 document.addEventListener("keydown", (e) => {
   document.getElementById("keyCode").innerHTML = e.key;
 });
-
 document.addEventListener("keypress", (e) => {
   document.getElementById("keyup").innerHTML = e.key;
 });
 
 /**
- * Android Interface
+ * Call
  */
-const register = () => {
-  return Android.register(SIP_SERVER, SIP_DOMAIN, EXTENSION_NUMBER, EXTENSION_PASSWORD);
-};
-
-const call = () => {
-  return Android.call(EXTENSION_NUMBER_);
-};
-
-const answer = () => {
-  return Android.answer();
-};
-
-const hangUp = () => {
-  return Android.hangUp();
-};
-
-const mute = () => {
-  return Android.mute();
-};
-
-const hold = () => {
-  return Android.hold();
-};
-
-
-document.addEventListener("click", (e) => {
-  const modal = document.getElementsByClassName("modal");
-  if (e.target == modal[0]) {
-    modal[0].style.display = "none";
-  }
-  if (e.target == modal[1]) {
-    modal[1].style.display = "none";
-  }
-  if (e.target == modal[2]) {
-    modal[2].style.display = "none";
+document.getElementById("call").addEventListener("click", (e) => {
+  if (Android.call(TARGET_EXTENSION_NUMBER)) {
+    document.getElementById("outgoingCall").style.display = "flex";
   }
 });
+
+const handleEndClick = (e) => {
+  if (Android.hangUp()) {
+    document.getElementById("outgoingCall").style.display = "none";
+    document.getElementById("duringCall").style.display = "none";
+  }
+};
+
+const handleHoldClick = (e) => {
+  const isChecked = e.target.checked;
+  if (!Android.hold(isChecked)) {
+    e.preventDefault();
+    e.target.checked = !isChecked;
+  }
+};
+
+const handleMuteClick = (e) => {
+  const isChecked = e.target.checked;
+  if (!Android.mute(isChecked)) {
+    e.preventDefault();
+    e.target.checked = !isChecked;
+  }
+};
+
+/**
+ * Incoming Call
+ */
+document.getElementById("incomingDecline").addEventListener("click", (e) => {
+  document.getElementById("incomingCall").style.display = "none";
+  return;
+});
+document.getElementById("incomingAccept").addEventListener("click", (e) => {
+  if (Android.answer()) {
+    document.getElementById("incomingCall").style.display = "none";
+    document.getElementById("duringCall").style.display = "flex";
+  }
+});
+
+/**
+ * Outgoing Call
+ */
+document.getElementById("outgoingMute").addEventListener("click", handleMuteClick);
+document.getElementById("outgoingEnd").addEventListener("click", handleEndClick);
+
+/**
+ * During Call
+ */
+document.getElementById("duringMute").addEventListener("click", handleMuteClick);
+document.getElementById("duringEnd").addEventListener("click", handleEndClick);
+document.getElementById("duringHold").addEventListener("click", handleHoldClick);
+
+/**
+ * Debug
+ */
 document.getElementById("debugOpenIncomingCall").addEventListener("click", (e) => {
   document.getElementById("incomingCall").style.display = "flex";
 });
